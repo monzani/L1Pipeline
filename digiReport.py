188bdc65203c311a4af24d193166b9d7549a9e0f
wai <>
2007-01-31 01:17:08 +0000

    converted Warren's digiReport.csh into a python version; cleaned up the variable defs in the xml file

---
+++ digiReport.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from os import system,environ
+ 
+system("cd ${TestDir}/${CHUNK_ID}; doxygen /afs/slac.stanford.edu/g/glast/ground/PipelineConfig/EM-tasks/builds/TestReport/v3r6p33/src/ReportDoxyfile ; mv *.eps latex/ ; cd latex ; latex refman.tex ; dvips -o refman.ps refman.dvi ; ps2pdf refman.ps refman.pdf; chgrp -R glast-pipeline '+environ['TestDir']+'/'+environ['CHUNK_ID']")
+
+ 
