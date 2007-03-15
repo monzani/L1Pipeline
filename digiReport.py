35d03b3b36d82a7ba3e0edb02a5590e66a367e69
wai <>
2007-03-15 20:46:29 +0000

    mods for adding in reconReport

--- digiReport.py
+++ digiReport.py
@@ -13,9 +13,10 @@ files = fileNames.setup(environ['DOWNLINK_ID'], environ['RUNID'], \
                         environ['CHUNK_ID'])
 
 staged = stageFiles.StageSet()
- 
-#system("cd "+files['dirs']['digiMon']+"; doxygen /afs/slac.stanford.edu/g/glast/ground/PipelineConfig/EM-tasks/builds/TestReport/v3r6p33/src/ReportDoxyfile ; mv *.eps latex/ ; cd latex ; latex refman.tex ; dvips -o refman.ps refman.dvi ; ps2pdf refman.ps refman.pdf; chgrp -R glast-pipeline "+environ['TestDir']+"/"+environ['CHUNK_ID'])
 
-system("cd "+files['dirs']['digiMon']+"; doxygen /afs/slac.stanford.edu/g/glast/ground/PipelineConfig/EM-tasks/builds/TestReport/v3r6p33/src/ReportDoxyfile ; mv *.eps latex/ ; cd latex ; latex refman.tex ; dvips -o refman.ps refman.dvi ; ps2pdf refman.ps refman.pdf; chgrp -R glast-pipeline "+files['dirs']['digiMon'])
+if environ['reportType'] == 'digi':
+ system("cd "+files['dirs']['digiMon']+"; doxygen /afs/slac.stanford.edu/g/glast/ground/PipelineConfig/EM-tasks/builds/TestReport/v3r6p33/src/ReportDoxyfile ; mv *.eps latex/ ; cd latex ; latex refman.tex ; dvips -o refman.ps refman.dvi ; ps2pdf refman.ps refman.pdf; chgrp -R glast-pipeline "+files['dirs']['digiMon'])
+else:
+ system("cd "+files['dirs']['reconMon']+"; doxygen /afs/slac.stanford.edu/g/glast/ground/PipelineConfig/EM-tasks/builds/TestReport/v3r6p33/src/ReportDoxyfile ; mv *.eps latex/ ; cd latex ; latex refman.tex ; dvips -o refman.ps refman.dvi ; ps2pdf refman.ps refman.pdf; chgrp -R glast-pipeline "+files['dirs']['reconMon'])
 
  
