a32be569bec753a4ff233b9d9f0ea05a036241da
focke <>
2018-10-17 03:16:00 +0000

    finish github build code

--- compile_stuff.py
+++ compile_stuff.py
@@ -14,7 +14,7 @@ import runner
 if len(sys.argv) > 1:
     names = sys.argv[1:]
 else:
-    names = config.cvsPackages.keys() + config.sConsPackages.keys() 
+    names = config.packages.keys()
     pass
 
 def doPackage(packName):
@@ -45,6 +45,41 @@ def doSConsPackage(packName):
     mkdir -p $(dirname $root)
     cd %(L1Build)s
     cvs co -r %(version)s -d %(root)s %(checkOutName)s
+    cd %(glastLocation)s 
+    %(scons)s --with-GLAST-EXT=%(glastExt)s --supersede %(L1Build)s --compile-opt %(packName)s 
+    ''' % args
+
+    runner.run(cmd)
+
+    if packName == "Monitor":
+        cmd = os.path.join(config.L1ProcROOT, 'compileRunStrip.py')
+        runner.run(cmd)
+        pass
+    return
+
+
+def doGithubPackage(packName):
+    package = config.packages[packName]
+
+    args = {
+        'L1Build': config.L1Build,
+        'glastLocation': config.glastLocation,
+        'glastExt': config.glastExt,
+        'scons': config.scons,
+        'packName': packName,
+        }
+    args.update(package)
+
+    args['source'] = '%(repository)s/archive/%(version)s.tar.gz' % args
+    args['directory'] = '%(packName)s-%(version)s' % args
+
+    cmd = '''
+    root=%(root)s
+    rm -rf $root
+    mkdir -p $(dirname $root)
+    cd %(L1Build)s
+    wget -O - %(source)s | tar xzv
+    mv %(directory)s %(packName)s
     cd %(glastLocation)s 
     %(scons)s --with-GLAST-EXT=%(glastExt)s --supersede %(L1Build)s --compile-opt %(packName)s 
     ''' % args
